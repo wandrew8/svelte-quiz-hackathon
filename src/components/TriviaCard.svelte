4b517daec35522385ff7c67ec73791b237e02008
Andrew Weiss <wandrew8@gmail.com>
2020-08-26 20:37:24 -0700

    adds new components

---
+++ src/components/TriviaCard.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  let trivia;
+
+  const loadTrivia = async () => {
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple"
+    );
+    const data = await res.json();
+    console.log(data);
+  };
+
+  onMount(() => {
+    trivie = loadTrivia;
+  });
+</script>
+
+<div>
+  <h1>Hello</h1>
+</div>
